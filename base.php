8ee3b793af0b004d70bb84c0b50bad9405a5ec97
Devin Cremins <drcremin@ncsu.edu>
2017-03-24 13:53:21 -0400

    IE footer fix take 1

--- base.php
+++ base.php
@@ -9,6 +9,7 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+    <div class="page-wrapper">
     <?php
       do_action('get_header');
       get_template_part('templates/header');
@@ -30,5 +31,6 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/footer');
       wp_footer();
     ?>
+  </div>
   </body>
 </html>
